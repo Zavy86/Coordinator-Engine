53fd5c689f3031ebc0297b5265bc75c8502afe49
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-02-15 16:30:42 +0100

    Error classes

---
+++ core/Error/Error.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Error
+ *
+ * @package Coordinator\Engine\Error
+ * @author Manuel Zavatta <manuel.zavatta@gmail.com>
+ */
+
+namespace Coordinator\Engine\Error;
+
+/**
+ * Error class
+ */
+final class Error extends AbstractError{
+
+	final public static function genericError(string $description,?string $information):static{
+		return new static(
+			'genericError',
+			$description,
+			$information
+		);
+	}
+}
