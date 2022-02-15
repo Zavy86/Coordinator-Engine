391469d8005b7067e1fa7a14376a0179c84b9d6b
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-02-15 16:03:15 +0100

    Bootstrap optimization

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -5,72 +5,27 @@
  * @package Coordinator\Engine     @todo valutare se spostare o togliere engine
  * @author Manuel Zavatta <manuel.zavatta@gmail.com>
  */
-
-// check for php version
-if(version_compare(PHP_VERSION,'8.1.0')<0){die('Required at least PHP version 8.0.0, current version: '.PHP_VERSION);}
-
-// check for debug                 @todo valutare se tenere
-global $debug;
-if(isset($_GET['debug']) && $_GET['debug']==1){$debug=true;}else{$debug=false;}
-
-// errors configuration
 error_reporting(E_ALL);
-ini_set('display_errors',$debug);
-
-/** @todo migliorare autoloader */
-
-// modules
-spl_autoload_register(function ($class){
-	//echo '<br>class '.$class.' auto-loading<br>';
-	// project-specific namespace prefix
-	$prefix='Coordinator\\Engine\\Modules\\';
-	// base directory for the namespace prefix
-	$base_dir=__DIR__.'/modules/';
-	// does the class use the namespace prefix?
-	$len = strlen($prefix);
-	if(strncmp($prefix, $class, $len)!==0){
-		// no, move to the next registered autoloader
-		return;
-	}
-	// get the relative class name
-	$relative_class=substr($class,$len);
-	// replace the namespace prefix with the base directory, replace namespace
-	// separators with directory separators in the relative class name, append
-	// with .php
-	$file=$base_dir.str_replace('\\','/',$relative_class).'.php';
-	// if the file exists, require it
-	if(file_exists($file)){
-		require $file;
-	}else{
-		throw new Exception('Error auto-loading class '.$class.'. File '.$file.' was not found');
-		die();
-	}
-});
-
-// core
-spl_autoload_register(function ($class){
-	//echo '<br>class '.$class.' auto-loading<br>';
-	// project-specific namespace prefix
-	$prefix='Coordinator\\Engine\\';
-	// base directory for the namespace prefix
-	$base_dir=__DIR__.'/core/';
-	// does the class use the namespace prefix?
-	$len = strlen($prefix);
-	if(strncmp($prefix, $class, $len)!==0){
-		// no, move to the next registered autoloader
-		return;
-	}
-	// get the relative class name
-	$relative_class=substr($class,$len);
-	// replace the namespace prefix with the base directory, replace namespace
-	// separators with directory separators in the relative class name, append
-	// with .php
-	$file=$base_dir.str_replace('\\','/',$relative_class).'.php';
-	// if the file exists, require it
-	if(file_exists($file)){
-		require $file;
-	}else{
-		throw new Exception('Error auto-loading class '.$class.'. File '.$file.' was not found');
-		die();
+ini_set('display_errors',(isset($_GET['debug']) && $_GET['debug']==1));
+if(version_compare(PHP_VERSION,'8.1.0')<0){die('Required at least PHP version 8.1.0, current version: '.PHP_VERSION);}
+spl_autoload_register(function($class){
+	//var_dump('Loading class '.$class);
+	$autoloadable_classes=array(
+		'Coordinator\Engine\Endpoints\\'=>'/endpoints/',
+		'Coordinator\Engine\\'=>'/core/'
+	);
+	foreach($autoloadable_classes as $prefix=>$path){
+		if(strncmp($prefix,$class,strlen($prefix))!==0){continue;}
+		$base_dir=__DIR__.$path;
+		$relative_class=substr($class,strlen($prefix));
+		$file=str_replace(['/','\\'],DIRECTORY_SEPARATOR,$base_dir.$relative_class.'.php');
+		if(!file_exists($file)){
+			if(isset($_GET['debug']) && $_GET['debug']==1){throw new Exception('Error auto-loading class '.$class.'. File '.$file.' was not found');}
+			header("Content-Type:application/json;charset=UTF-8");
+			http_response_code(500);
+			die(json_encode("500 Internal server error"));
+		}
+		require_once($file);
+		break;
 	}
 });
