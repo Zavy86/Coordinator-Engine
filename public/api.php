9cb0bc6189d6694af1829c00a297d231c3661f9e
Manuel Zavatta <manuel.zavatta@gmail.com>
2022-02-15 16:40:29 +0100

    Engine entry point

--- public/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * API
- *
- * @package Coordinator\Engine
- * @author Manuel Zavatta <manuel.zavatta@gmail.com>
- */
-require_once("../bootstrap.inc.php");
-\Coordinator\Engine\Engine::run();
