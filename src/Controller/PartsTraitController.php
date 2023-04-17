88a7f7ce8c4a4b970d48ad28f9af4c88e95a518d
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-04-17 14:08:13 +0200

    add parts trait for controllers

---
+++ src/Controller/PartsTraitController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Parts Trait Controller
+ *
+ * @package Coordinator\Engine
+ * @author Manuel Zavatta <manuel.zavatta@gmail.com>
+ */
+
+namespace Coordinator\Engine\Controller;
+
+trait PartsTraitController{
+
+	/**
+	 * @var string[] requested parts
+	 */
+	private array $_parts=[];
+
+	/**
+	 * load parts from request
+	 *
+	 * @return void
+	 */
+	private function _loadParts():void{
+		$this->_parts=[];
+		$query=$this->Request->getQuery();
+		if(array_key_exists('parts',$query) && strlen($query['parts'])){
+			foreach(explode(',',$query['parts']) as $part){
+				if(!strlen($part)){continue;}
+				$this->_parts[]=strtolower($part);
+			}
+		}
+	}
+
+	/**
+	 * get requested parts
+	 *
+	 * @return string[]
+	 */
+	private function getParts():array{
+		if(empty($this->_parts)){$this->_loadParts();}
+		return $this->_parts;
+	}
+
+	/**
+	 * check if a specific part has requested
+	 *
+	 * @param string $part
+	 * @return bool
+	 */
+	private function hasPart(string $part):bool{
+		return in_array(strtolower($part),$this->getParts());
+	}
+
+}
