7364e695a394810ddd8134328a5c35378491c967
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-06-21 09:32:22 +0200

    add generic exception

--- src/Object/ObjectException.php
+++ src/Object/ObjectException.php
@@ -10,6 +10,10 @@ namespace Coordinator\Engine\Object;
 
 final class ObjectException extends \Exception{
 
+	final public static function genericException(string $exception):static{
+		return new static($exception);
+	}
+
 	public static function propertyNotExists(string $class,string $property):static{
 		return new static('Property `'.$property.'` does not exists in model `'.$class.'`.');
 	}
