6e96f5d44df092bdfd42785a6e3c1c312aa370fb
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-09-18 16:08:30 +0200

    add generic controller exception

--- src/Controller/ControllerException.php
+++ src/Controller/ControllerException.php
@@ -10,4 +10,8 @@ namespace Coordinator\Engine\Controller;
 
 final class ControllerException extends \Exception{
 
+	final public static function genericException(string $exception):static{
+		return new static($exception);
+	}
+
 }
